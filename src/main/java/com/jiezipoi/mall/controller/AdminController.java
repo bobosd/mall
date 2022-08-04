13be074c8632bacd06305f25882dc231ac9970be
JieZi <867664318@qq.com>
2022-08-04 03:09:10 +0200

    Creating login panel for dashboard, initial commit.

--- src/main/java/com/jiezipoi/mall/controller/AdminController.java
+++ src/main/java/com/jiezipoi/mall/controller/AdminController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
     @GetMapping("/index")
     public String index() {
         return "admin/admin-index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "admin/login";
     }
 }
